efedd2428028a48bc45906fd428d9d7614dd98a2
Ivan Korolkov <korolkov.ivan@mail.ru>
2024-08-04 20:41:14 +0300

    Added function prototypes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <windows.h>
+#include <iostream>
 
 // Глобальные переменные
 const char g_szClassName[] = "MainWindowClass";
 
 // Прототипы функций
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// Функции для работы с COM-портом
+bool OpenCOMPort(const char* portName);
+void CloseCOMPort();
+bool ConfigureCOMPort(int baudRate, int byteSize, int parity, int stopBits);
+bool WriteToCOMPort(const char* command);
+bool ReadFromCOMPort(char* buffer, int bufferSize);
+
+// Функции для управления источником питания
+bool SetVoltage(double voltage);
+bool SetCurrent(double current);
+bool SetRiseTime(double riseTime);
+bool SetFallTime(double fallTime);
+bool SetOnDelay(double onDelay);
+bool SetOffDelay(double offDelay);
+bool EnableCurrentProtection(bool enable);
+bool EnableVoltageProtection(bool enable);
+bool SaveSettings(int memoryBank);
+
+// Функции для чтения и регистрации значений
+bool GetVoltage(double& voltage);
+bool GetCurrent(double& current);
+void RegisterMinMaxValues(double voltage, double current);
+
+// Функции для графического интерфейса
+void CreateMainWindow();
+void UpdateDisplay();
+void VisualizeCurrent();
+void UpdateLED(int ledID, int status);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     WNDCLASSEX wc;
@@ -69,5 +99,109 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
+}
+
+
+// Функции для работы с COM-портом
+bool OpenCOMPort(const char* portName) {
+    std::cout << "OpenCOMPort" << std::endl;
+    return true;
+}
+
+void CloseCOMPort() {
+    std::cout << "CloseCOMPort" << std::endl;
+}
+
+bool ConfigureCOMPort(int baudRate, int byteSize, int parity, int stopBits) {
+    std::cout << "ConfigureCOMPort" << std::endl;
+    return true;
+}
+
+bool WriteToCOMPort(const char* command) {
+    std::cout << "WriteToCOMPort" << std::endl;
+    return true;
+}
+
+bool ReadFromCOMPort(char* buffer, int bufferSize) {
+    std::cout << "ReadFromCOMPort" << std::endl;
+    return true;
+}
+
+// Функции для управления источником питания
+bool SetVoltage(double voltage) {
+    std::cout << "SetVoltage" << std::endl;
+    return true;
+}
+
+bool SetCurrent(double current) {
+    std::cout << "SetCurrent" << std::endl;
+    return true;
+}
+
+bool SetRiseTime(double riseTime) {
+    std::cout << "SetRiseTime" << std::endl;
+    return true;
+}
+
+bool SetFallTime(double fallTime) {
+    std::cout << "SetFallTime" << std::endl;
+    return true;
+}
+
+bool SetOnDelay(double onDelay) {
+    std::cout << "SetOnDelay" << std::endl;
+    return true;
+}
+
+bool SetOffDelay(double offDelay) {
+    std::cout << "SetOffDelay" << std::endl;
+    return true;
+}
+
+bool EnableCurrentProtection(bool enable) {
+    std::cout << "EnableCurrentProtection" << std::endl;
+    return true;
+}
+
+bool EnableVoltageProtection(bool enable) {
+    std::cout << "EnableVoltageProtection" << std::endl;
+    return true;
+}
+
+bool SaveSettings(int memoryBank) {
+    std::cout << "SaveSettings" << std::endl;
+    return true;
+}
+
+// Функции для чтения и регистрации значений
+bool GetVoltage(double& voltage) {
+    std::cout << "GetVoltage" << std::endl;
+    return true;
+}
+
+bool GetCurrent(double& current) {
+    std::cout << "GetCurrent" << std::endl;
+    return true;
+}
+
+void RegisterMinMaxValues(double voltage, double current) {
+    std::cout << "RegisterMinMaxValues" << std::endl;
+}
+
+// Функции для графического интерфейса
+void CreateMainWindow() {
+    std::cout << "CreateMainWindow" << std::endl;
+}
+
+void UpdateDisplay() {
+    std::cout << "UpdateDisplay" << std::endl;
+}
+
+void VisualizeCurrent() {
+    std::cout << "VisualizeCurrent" << std::endl;
+}
+
+void UpdateLED(int ledID, int status) {
+    std::cout << "UpdateLED" << std::endl;
 }
 
